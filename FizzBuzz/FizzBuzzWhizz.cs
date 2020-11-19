1faa5bc4c3fa60885c20ac05b8d73c6b25ef602c
Ge Luxue <lge5@slb.com>
2020-11-19 17:09:22 +0800

    test: add test

---
+++ FizzBuzz/FizzBuzzWhizz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzWhizz
+{
+    public class FizzBuzzWhizz
+    {
+        public static string Say(int count)
+        {
+            return "1";
+        }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzWhizzTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FizzBuzzWhizz;
+using Moq;
+using Xunit;
+
+namespace FizzBuzzWhizzTest
+{
+    public class FizzBuzzWhizzTest
+    {
+        [Fact]
+        public void Should_Return_1_Given_1()
+        {
+            //given
+            int given = 1;
+
+            //when
+            string actual = FizzBuzzWhizz.FizzBuzzWhizz.Say(given);
+            string expected = "1";
+
+            //then
+            Assert.Equal(actual, expected);
+        }
+    }
+}
